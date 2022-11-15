25d37133c97bcd16560ce72cbf42ee5ff96ef6f0
Mauricio Hernan Cabrera <mauriciohernancabrera@gmail.com>
2022-11-15 11:00:04 -0300

    updated setting fleek 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: "/" }
+    }
+  }
 }
 
 module.exports = nextConfig
